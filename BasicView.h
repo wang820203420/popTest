2396c3e52726bcd4f395b4388ba37eb2dca36722
wang820203420 <zhisu@zhisudeMacBook-Pro.local>
2015-07-03 11:44:54 +0800

    new

---
+++ BasicView.h
@@ -0,0 +1,13 @@
+//
+//  BasicView.h
+//  popTest
+//
+//  Created by zhisu on 15/6/30.
+//  Copyright (c) 2015年 zhisu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BasicView : UIView
+
+@end
